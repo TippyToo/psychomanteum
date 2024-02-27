9e1e7b4c31b6c22f4791620e92091b2ac0bdc673
MrSwoops <rmcooke33@gmail.com>
2024-02-27 14:56:48 -0500

    removed unnecessary save data from testing and clarified menu options

--- Project Pyschomanteum/Assets/Scripts/SaveLoad/SaveData.cs
+++ Project Pyschomanteum/Assets/Scripts/SaveLoad/SaveData.cs
@@ -6,18 +6,13 @@ using UnityEngine;
 public class SaveData
 {
     public int chapter;
-    public string saveImagePath;
-    //public Sprite saveImage = Resources.Load<Sprite>("Sprites/carrots");
 
     //Add things that would need to be saved here. Including but is not limited to journal entries, dialogue, checkpoints, inventory, world falgs, etc.
     public SerializableDictionary<string, int> currentConversation;
-    public int conversationToLoad;
     public Vector3 playerPosition;
 
     public SaveData() {
         chapter = 1;
-        saveImagePath = "Sprites/carrots";
-        conversationToLoad = 0;
         currentConversation = new SerializableDictionary<string, int>();
     }
 }
