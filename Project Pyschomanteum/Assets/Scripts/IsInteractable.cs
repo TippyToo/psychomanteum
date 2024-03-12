68c1535765e9df0c7d57606484e4a8fc6e896dc1
MrSwoops <rmcooke33@gmail.com>
2024-03-12 15:30:06 -0400

    Added NPC interact images
    When close enough to talk to an NPC, an image will let you know

---
+++ Project Pyschomanteum/Assets/Scripts/IsInteractable.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IsInteractable : MonoBehaviour
+{
+
+    public float fadeTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //Interact indicator fade in
+    private IEnumerator FadeIn(SpriteRenderer interactSprite)
+    {
+        Color temp = interactSprite.color;
+        while (temp.a < 1.0f)
+        {
+            temp.a += Time.deltaTime / fadeTime;
+            if (temp.a >= 1.0f)
+                temp.a = 1.0f;
+            interactSprite.color = temp;
+            yield return null;
+        }
+    }
+
+    //Interact indicator fade out
+    private IEnumerator FadeOut(SpriteRenderer interactSprite)
+    {
+        Color temp = interactSprite.color;
+        while (temp.a > 0.0f)
+        {
+            temp.a -= Time.deltaTime / fadeTime;
+            if (temp.a <= 0.0f)
+                temp.a = 0.0f;
+            interactSprite.color = temp;
+            yield return null;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StopAllCoroutines();
+            StartCoroutine(FadeIn(GetComponent<SpriteRenderer>()));
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StopAllCoroutines();
+            StartCoroutine(FadeOut(GetComponent<SpriteRenderer>()));
+        }
+    }
+}
--- Project Pyschomanteum/Assets/Scripts/SceneTransition.cs
+++ Project Pyschomanteum/Assets/Scripts/SceneTransition.cs
@@ -9,7 +9,7 @@ public class SceneTransition : MonoBehaviour
     [Tooltip("Set to require input to transfer scenes. False automatically changes scenes when the player gets close enough")]
     public bool interactable;
     public string sceneNameToLoad = "MainMenu";
-    public float fadeTime;
+    
     private bool detectsPlayer = false;
 
 
@@ -22,40 +22,12 @@ public class SceneTransition : MonoBehaviour
         }
     }
 
-    //Interact indicator fade in
-    private IEnumerator FadeIn(SpriteRenderer interactSprite) {
-        Color temp = interactSprite.color;
-        while (temp.a < 1.0f) {
-            temp.a += Time.deltaTime / fadeTime;
-            if (temp.a >= 1.0f)
-                temp.a = 1.0f;
-            interactSprite.color = temp;
-            yield return null;
-        }
-    }
-
-    //Interact indicator fade out
-    private IEnumerator FadeOut(SpriteRenderer interactSprite)
-    {
-        Color temp = interactSprite.color;
-        while (temp.a > 0.0f)
-        {
-            temp.a -= Time.deltaTime / fadeTime;
-            if (temp.a <= 0.0f)
-                temp.a = 0.0f;
-            interactSprite.color = temp;
-            yield return null;
-        }
-    }
-
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player") { 
             detectsPlayer = true;
-            if (interactable) {
-                StopAllCoroutines();
-                StartCoroutine(FadeIn(GetComponent<SpriteRenderer>()));
-            } else if (!interactable) {
+            if (!interactable) {
                 GameObject.Find("Save Manager").GetComponent<SaveManager>().SaveGame();
                 SceneManager.LoadScene(sceneNameToLoad);
             }
@@ -65,11 +37,6 @@ public class SceneTransition : MonoBehaviour
     {
         if (other.tag == "Player") { 
             detectsPlayer = false;
-            if (interactable)
-            {
-                StopAllCoroutines();
-                StartCoroutine(FadeOut(GetComponent<SpriteRenderer>()));
-            }
         }
     }
 }
