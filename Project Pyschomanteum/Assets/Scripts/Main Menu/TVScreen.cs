2635edb35cb96d0136f511fadac159e8cd3e25ac
TippyToo <wm.titus2@gmail.com>
2024-04-16 15:53:14 -0400

    Update Settings
    Settings now uses the VCR font, and functions properly

--- Project Pyschomanteum/Assets/Scripts/Main Menu/TVScreen.cs
+++ Project Pyschomanteum/Assets/Scripts/Main Menu/TVScreen.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class TVScreen : MonoBehaviour
 {
     Stack<GameObject> tvScreen = new Stack<GameObject>();
     private int saveSlots = 3;
+
+    // Settings variables
+    public Slider musicVolumeSlider;
+    public Slider dialogueVolumeSlider;
+    public Slider textSpeedSlider;
+    public Text musicText;
+    public Text dialogueText;
+    public Text textSpeedText;
+
 
     // Stack functions
     public void Push(GameObject screen)
@@ -33,6 +42,8 @@ public class TVScreen : MonoBehaviour
         Application.Quit();
     }
 
+
+    // Save selection functions
     public void DecideLoadButtonBehaviors()
     {
         for (int i = 0; i < saveSlots; i++)
@@ -41,4 +52,44 @@ public class TVScreen : MonoBehaviour
         }
     }
 
+
+    // Settings functions
+
+    // "category" should be the one word, capitalized volume type this slider adjusts. i.e. "Music"
+    public void SaveVolume(string category)
+    {
+        switch (category)
+        {
+            case "Music":
+                PlayerPrefs.SetFloat(category, musicVolumeSlider.value);
+                musicText.text = ((int)(musicVolumeSlider.value * 100)).ToString() + "%";
+                break;
+            case "Dialogue":
+                PlayerPrefs.SetFloat(category, dialogueVolumeSlider.value);
+                dialogueText.text = ((int)(dialogueVolumeSlider.value * 100)).ToString() + "%";
+                break;
+        }
+    }
+
+    public void SaveTextSpeed()
+    {
+        // Save setting
+        PlayerPrefs.SetInt("Text Speed", ((int)textSpeedSlider.value));
+
+        // Change displayed value
+        string temp = "";
+        switch (textSpeedSlider.value)
+        {
+            case 1:
+                temp = "Slow";
+                break;
+            case 2:
+                temp = "Medium";
+                break;
+            case 3:
+                temp = "Fast";
+                break;
+        }
+        textSpeedText.text = temp;
+    }
 }
